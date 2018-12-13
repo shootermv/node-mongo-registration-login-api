e5d0aa5f0304f05997ea5f327635e819220b53e1
Moshe Vilner <moshevilner@wipoferxps.webintpro.com>
2018-12-13 15:10:07 +0200

    socket listener added

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 ﻿require('rootpath')();
 const express = require('express');
 const app = express();
-const socketIO = require('socket.io')
+const socketIo = require('socket.io')
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const jwt = require('_helpers/jwt');
@@ -25,4 +25,14 @@ app.use(errorHandler);
 const port = process.env.NODE_ENV === 'production' ? 80 : 4000;
 const server = app.listen(port, function () {
     console.log('Server listening on port ' + port);
+});
+
+const io = socketIo(server);
+io.on("connection", socket => {
+  console.log("New client connected"), 
+  setInterval(
+    () => console.log("something happens..."),
+    10000
+  );
+  socket.on("disconnect", () => console.log("Client disconnected"));
 });
